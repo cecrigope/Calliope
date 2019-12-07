aea8211aaf0d174d9389fcac7a8389e530c63b82
cecrigope <cecrigope@hotmail.com>
2019-12-07 01:15:32 -0500

    Users top tracks now include track statistics

--- client/src/pages/TopTracks.js
+++ client/src/pages/TopTracks.js
@@ -16,6 +16,7 @@ import {
   MDBJumbotron
 } from "mdbreact";
 import SpotifyAPI from "spotify-web-api-js";
+import _ from "lodash";
 
 function TopTracks() {
   const [openModal, setOpenModal] = useState(false);
@@ -34,12 +35,16 @@ function TopTracks() {
 
     if (tracks === false) {
       spotify.getMyTopTracks(options, function(err, data) {
-        console.log(data.items);
-        setTracks(data.items);
+        var trackIds = _.map(data.items, "id");
+
+        spotify.getAudioFeaturesForTracks(trackIds, function(err, trackData) {
+          var merge = _.merge(data.items, trackData.audio_features);
+          setTracks(merge);
+        });
       });
     }
   }
-
+  
   return (
     <div className="body">
       <MDBContainer>
@@ -52,30 +57,87 @@ function TopTracks() {
           {tracks &&
             tracks.map(track => (
               <MDBCol sm={12}>
-                <MDBJumbotron className="jumbotron-track">
-                  <MDBRow>
-                    <MDBCol md={3}>
-                      <img className="img-fluid" src={track.album.images[0].url} />
-                    </MDBCol>
-                    <MDBCol md={9}>
-                      <h2>{track.name}</h2>
-                      <p className="no-margin">
-                        {track.artists.length > 1 ? "Artists" : "Artist"}:{" "}
-                        {track.artists.length > 1
-                          ? track.artists.map((artist, index) =>
-                              index + 1 === track.artists.length
-                                ? artist.name
-                                : artist.name + ", "
-                            )
-                          : track.artists[0].name}
-                      </p>
-                      <p className="no-margin">Album: {track.album.name}</p>
-                      <p className="no-margin">
-                        Popularity Score: {track.popularity}
-                      </p>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBJumbotron>
+                <div className="card" style={{ marginBottom: 16 }}>
+                  <div className="row no-gutters">
+                    <div className="col-lg-3 col-md-4" style={{ background: `#868e96` }}>
+                      <img
+                        src={track.album.images[0].url}
+                        className="card-img-top h-100 card-img-track"
+                        alt="..."
+                      />
+                    </div>
+                    <div className="col-lg-9 col-md-8">
+                      <div className="card-body">
+                        <h2 className="card-title">{track.name}</h2>
+                        <div className="card-text">
+                          <div className="row no-gutters">
+                            <div className="col-md-6 col-sm-12">
+                              <p className="no-margin">
+                                <span className="primary-text">
+                                  {track.artists.length > 1
+                                    ? "Artists"
+                                    : "Artist"}
+                                  :{" "}
+                                </span>
+                                {track.artists.length > 1
+                                  ? track.artists.map((artist, index) =>
+                                      index + 1 === track.artists.length
+                                        ? artist.name
+                                        : artist.name + ", "
+                                    )
+                                  : track.artists[0].name}
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">Album: </span>
+                                {track.album.name}
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">
+                                  Popularity Score:
+                                </span>{" "}
+                                {track.popularity}
+                              </p>
+                            </div>
+                            <div className="col-md-6 col-sm-12">
+                              <p className="no-margin">
+                                <span className="primary-text">
+                                  Acousticness:{" "}
+                                </span>
+                                {Math.round(track.acousticness * 100)}%
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">
+                                  Danceability:{" "}
+                                </span>
+                                {Math.round(track.danceability * 100)}%
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">Energy: </span>
+                                {Math.round(track.energy * 100)}%
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">
+                                  Instrumentalness:{" "}
+                                </span>
+                                {Math.round(track.instrumentalness * 100)}%
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">Liveness: </span>
+                                {Math.round(track.liveness * 100)}%
+                              </p>
+                              <p className="no-margin">
+                                <span className="primary-text">
+                                  Speechiness:{" "}
+                                </span>
+                                {Math.round(track.speechiness * 100)}%
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </MDBCol>
             ))}
         </MDBRow>
