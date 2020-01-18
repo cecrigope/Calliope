ab9f5d5a86545ba9cc3e685220d587426d60be41
cecrigope <cecrigope@hotmail.com>
2020-01-18 11:04:09 -0500

    Modified login uri for heroku deployment

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-const LOGIN_URI = "http://localhost:8888/login";
+const LOGIN_URI = process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:8888/login'
+    : 'https://calliope-spotify.herokuapp.com/login';;
 
 const HomePage = () => (
   <div className="home bg-dark">
@@ -9,7 +11,7 @@ const HomePage = () => (
         <div className="w-full text-center">
           <h1 className="text-primary text-7xl mb-8">Calliope</h1>
           <a
-            href="http://localhost:8888/login"
+            href={LOGIN_URI}
             class="bg-transparent hover:bg-primary text-primary hover:text-white border border-primary py-2 px-6 rounded-lg text-2xl uppercase"
           >
             Log In to Spotify
